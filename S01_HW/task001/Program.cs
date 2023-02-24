57b1da79b1c8fe22f5b41947752d32daf3518591
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-24 19:49:50 +0300

    created S01 homework + talk001

---
+++ S01_HW/task001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int GetInput(){
+    string? tmp = null;
+    while (tmp == null){
+        tmp = Console.ReadLine();
+        if (tmp == null){
+            Console.WriteLine("Nullable vaules are forboted");
+        }
+    }
+    int res = Convert.ToInt32(tmp);
+    return res;
+}
+void Print(string arg, Boolean wrapDont){
+    if (wrapDont) {
+        Console.Write(arg);
+    }else {
+        Console.WriteLine(arg);
+    }
+}
+
+Console.Clear();
+Print("a=",true);
+int a = GetInput();
+Print("b=",true);
+int b = GetInput();
+int max = a;
+if (b > a) {
+    max = b;
+}
+Print($" a = {a}; b = {b}; max = {Convert.ToString(max)}",false);
--- S01_examples/Program.cs
+++ S01_examples/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args){
+        int GetInput(){
+            string tmp = Console.ReadLine();
+            if (tmp == null){
+                Console.WriteLine("Nullable vaules are forboted");
+            }
+            int res = Convert.ToInt32(tmp);
+            return res;
+        }
+        
+        Console.Write(" Input new value:");
+        int num = GetInput();
+        num = num * num;
+        Console.Clear();
+        Console.WriteLine("Res: " + num);
+
+    }
+}
