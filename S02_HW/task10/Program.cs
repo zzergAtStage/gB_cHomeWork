e74b2e781e03d5cedceace03ea8e927048254642
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-27 21:03:28 +0100

    added gitignore

---
+++ S02_HW/task10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task10;
+class Program
+{
+    static int GetInputInt(){
+        int res = Random.Shared.Next(100,1000);
+        //int 
+        return res; 
+    }
+    static void Main(string[] args)
+    {
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+       Console.Clear();
+       int number = GetInputInt();
+       //получаем второе число
+       int secondNum = ((number - (number % 10)) / 10) % 10;
+       Console.WriteLine($" Number = {number}, second digit ={secondNum}");
+
+    }
+}
