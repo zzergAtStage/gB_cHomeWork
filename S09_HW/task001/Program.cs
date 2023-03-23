d083162674b5eca9097f23e8900f862836df7dc1
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-03-23 23:49:04 +0100

    S09 add task001 debugs

---
+++ S09_HW/task001/Program.cs
@@ -0,0 +1,36 @@
+﻿using Libraries;
+namespace task001;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        int leftRange = Library.GetInputFromUser("Input left range of natural numbers (m)");
+        int rightRange = Library.GetInputFromUser("Input right range of natural numbers (N)");
+
+        string result = GetEvenNaturalNumbersToStoring(leftRange, rightRange);
+        System.Console.WriteLine($" M,N = [{leftRange}],[{rightRange}] -> {result}");
+    }
+    static string GetEvenNaturalNumbersToStoring(int pLeftRange, int pRightRange)
+    {
+        string tmpResult = String.Empty;
+        System.Console.Write($"L={pLeftRange}=>");
+        if (pLeftRange >= pRightRange) return tmpResult;
+
+
+        if (pLeftRange % 2 == 0)
+        {
+            Environment.Exit(0);
+            System.Console.Write($"L={pLeftRange}=>");
+            return $"{tmpResult}, {GetEvenNaturalNumbersToStoring(pLeftRange++, pRightRange)}";
+            
+        }
+        else
+        {   
+            System.Console.Write($"L={pLeftRange}=>");
+            Environment.Exit(0);
+            return GetEvenNaturalNumbersToStoring(pLeftRange++, pRightRange);
+            
+        }
+    }
+}
