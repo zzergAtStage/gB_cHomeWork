ce58cb9e86aa25d7db831485e955cc1e6d0e4c72
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-03-24 00:12:50 +0100

    S09 add task001 increment strange behavior

--- S09_HW/task001/Program.cs
+++ S09_HW/task001/Program.cs
@@ -8,24 +8,28 @@ class Program
         int leftRange = Library.GetInputFromUser("Input left range of natural numbers (m)");
         int rightRange = Library.GetInputFromUser("Input right range of natural numbers (N)");
 
-        string result = GetEvenNaturalNumbersToStoring(leftRange, rightRange);
+        string result = OutputEvenNaturalNumbersToStoring(leftRange, rightRange);
         System.Console.WriteLine($" M,N = [{leftRange}],[{rightRange}] -> {result}");
     }
-    static string GetEvenNaturalNumbersToStoring(int pLeftRange, int pRightRange)
+    static string OutputEvenNaturalNumbersToStoring(int pLeftRange, int pRightRange)
     {
         string tmpResult = String.Empty;
         if (pLeftRange >= pRightRange)
         {
             return tmpResult;
         }
-        pLeftRange++;
+        //pLeftRange++; why doesn't work increment in the methods call
         if (pLeftRange % 2 == 0)
         {
-            return tmpResult + $"{pLeftRange}, {GetEvenNaturalNumbersToStoring(pLeftRange, pRightRange)}";
+            if (pLeftRange == pRightRange) { //i hate non beautified output
+                return tmpResult + $"{pLeftRange} {OutputEvenNaturalNumbersToStoring(pLeftRange, pRightRange)}";
+            } else {
+                return tmpResult + $"{pLeftRange}, {OutputEvenNaturalNumbersToStoring(pLeftRange, pRightRange)}";
+            }
         }
         else
         {
-            return GetEvenNaturalNumbersToStoring(pLeftRange, pRightRange);
+            return OutputEvenNaturalNumbersToStoring(pLeftRange, pRightRange);
         }
     }
 }
